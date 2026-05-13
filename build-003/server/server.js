6fc4a95c5611a0aa9827a1a261a5adf67b9e8d2b
Kwamz3 <charlie18appiah@gmail.com>
2026-05-13 12:25:43 +0000

    feat: initialize React app with Vite and Socket.io server
    - Added React and Vite setup with main entry point and CSS styles.
    - Included SVG assets for React and Vite logos.
    - Created a basic Socket.io server with CORS configuration.
    - Added package.json and package-lock.json for server dependencies.
    - Configured .gitignore for server files and logs.

---
+++ build-003/server/server.js
@@ -0,0 +1,18 @@
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const PORT = 3000;
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+        origin:"http://localhost:5173"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port:`, `http://localhost:${PORT}`)
+})
